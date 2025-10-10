6c7cd87a174a162cb6802c712cc3e2380791a59c
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2025-10-09 17:44:28 -0700

    excel files are importing correctly

---
+++ script.py
@@ -0,0 +1,27 @@
+import argparse
+import csv
+import pandas as pd
+import pymongo
+
+parser = argparse.ArgumentParser()
+
+# Flags created to handle different actions and database calls
+parser.add_argument("-file", action="store", required=True, help="Input file")
+parser.add_argument("-verbose", action="store_true", help="Verbose output")
+parser.add_argument("-kevin", action="store", help=" Get a reportt of all \"Work\" that Kevin has done")
+parser.add_argument("-rbugs", help="Lists all repeatable bugs")
+parser.add_argument("-bbugs", help="Lists all blocker bugs")
+parser.add_argument("-RBbugs", help="Lists all repeatable and blocker bugs")
+parser.add_argument("-date", action="store", help="Lists all reports on build 2/24/2024")
+
+args = parser.parse_args()
+
+try:
+    df = pd.read_excel(args.file)
+    if args.verbose:
+        print(df.to_string(index=False))
+    print("File imported!")
+except FileNotFoundError:
+    print("File not found")
+    exit(1)
+
