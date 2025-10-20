da03feaaab95c1db2296efc8c1882e6dbe5480e2
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2025-10-20 12:38:40 -0700

    reworked db calls

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 import argparse
+import sys
+
 import pandas as pd
+from pandas import ExcelWriter
 from pymongo import MongoClient
 import os
 
@@ -9,9 +12,9 @@ parser = argparse.ArgumentParser()
 # Flags created to handle different actions and database calls
 # action= store => argument required
 # action = store_true => no argument required
-parser.add_argument("-file", action="store", required=True, help="Input file")
-parser.add_argument("-verbose", action="store_true", help="Verbose output")
-parser.add_argument("-kevin",action="store_true", help=" Get a report of all \"Work\" that Kevin has done")
+parser.add_argument("--file", action="store", required=False, help="Input file to create database")
+parser.add_argument("--verbose", action="store_true", help="Verbose output")
+parser.add_argument("--user",action="store", help=" Get a report of a specified user")
 parser.add_argument("-rbugs", action="store_true", help="Lists all repeatable bugs")
 parser.add_argument("-bbugs", action="store_true", help="Lists all blocker bugs")
 parser.add_argument("-RBbugs", action="store_true", help="Lists all repeatable and blocker bugs")
@@ -19,36 +22,53 @@ parser.add_argument("-date", action="store_true", help="Lists all reports on bui
 
 args = parser.parse_args()
 
+all_data = []
+
 try:
-    df = pd.read_excel(args.file)
 
-    # This handles creating and accessing correct database based on which file is passed through
-    filename = os.path.basename(args.file)
-    collection_name = filename.split(".")[0]
-    print("Collection Name:", collection_name)
-
-    # Create connection with MongoDB
+    # Connecting to db
     client = MongoClient('localhost', 27017)
     database = client["qa_logs"]
-    collection = database[collection_name]
 
-    # Inserting data to db
-    data = df.to_dict('records')
-    collection.insert_many(data)
-    print(f"Successfully inserted data into {collection_name} database")
+    # Available databases
+    if args.file:
+        df = pd.read_excel(args.file)
+
+        # This handles creating and accessing correct database based on which file is passed through
+        filename = os.path.basename(args.file)
+        collection_name = filename.split(".")[0]
+        collection = database[collection_name]
+
+        # Insert data to collection
+        data = df.to_dict('records')
+        collection.insert_many(data)
+        print(f"Successfully inserted data into {collection_name} collection")
+        sys.exit(0)
+
+
+    list_of_tables = database.list_collection_names();
+    for table in list_of_tables:
+        collection = database[table]
+        print("Contents:")
+        for row in collection.find({}):
+            all_data.append(row)
+        print(all_data)
 
     #  Database Calls
-    if args.kevin:
+    # Need to modify to take in a string as an input
+    if args.user:
         report = collection.find({"Test Owner": "Kevin Chaja"})
         print(f"Successfully found all of Kevin's entries in {collection_name} database")
+        try:
+            export_file = open("Chaja's work.xlsx", "wb")
+        except FileNotFoundError:
+            print("File not found")
         for r in report:
-            print(r)
-            print("\n")
+            r.to_excel(export_file)
 
     if args.verbose:
         print(df.to_string(index=False))
 
-    print("File imported!")
 except FileNotFoundError:
     print("File not found")
     exit(1)
