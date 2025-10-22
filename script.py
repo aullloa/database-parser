ff6e963b6f003875b89a5493addace855bec6245
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2025-10-22 12:47:28 -0700

    refined args.user call

--- script.py
+++ script.py
@@ -10,13 +10,14 @@ parser = argparse.ArgumentParser()
 # Flags created to handle different actions and database calls
 # action= store => argument required
 # action = store_true => no argument required
-parser.add_argument("--file", action="store", required=False, help="Input file to create database")
+parser.add_argument("--file", action="store", required=False, help="Input file to create database from contents")
 parser.add_argument("--verbose", action="store_true", help="Verbose output")
-parser.add_argument("--user",action="store", help=" Get a report of a specified user")
+parser.add_argument("--user",action="store", help="Get a report of a specified user")
 parser.add_argument("--repeat", action="store_true", help="Lists all repeatable bugs")
 parser.add_argument("--blocker", action="store_true", help="Lists all blocker bugs")
 parser.add_argument("--rb", action="store_true", help="Lists all repeatable and blocker bugs")
-parser.add_argument("--date", action="store", help="Lists all reports on given buikld date")
+parser.add_argument("--date", action="store", help="Lists all reports on given build date")
+parser.add_argument("--debug", action="store_true", help="Debug mode")
 
 args = parser.parse_args()
 
@@ -45,8 +46,9 @@ try:
         print(f"Successfully inserted data into {collection_name} collection")
         sys.exit(0)
 
-
-    list_of_tables = database.list_collection_names();
+    # Need to add further checks to catch incorrect data  e.g. incorrect data format, inconsistent data in wrong columns
+    # Date: need to convert to readable data
+    list_of_tables = database.list_collection_names()
     for table in database.list_collection_names():
         collection = database[table]
         for row in collection.find({}):
@@ -55,10 +57,11 @@ try:
                 data_tracker.add(key)
                 all_data.append(row)
 
-    #DEBUG: to check data has no duplicates
     df = pd.DataFrame(all_data)
-    df.to_excel("no_dupes.xlsx", index=False)
-    print("No dupe data exported to no_dupes.xlsx")
+    #DEBUG: to check data has no duplicates
+    if args.debug:
+        df.to_excel("no_dupes.xlsx", index=False)
+        print("no_dupes.xlsx created")
     # END DEBUG
 
     #  Database Calls
@@ -67,14 +70,15 @@ try:
         print(df.to_string(index=False))
 
     if args.user:
-        report = collection.find({"Test Owner": "Kevin Chaja"})
-        print(f"Successfully found all of Kevin's entries in {collection_name} database")
+        username = args.user
+        report = df[df["Test Owner"] == username]
+        print(f"Successfully found all of {username}'s entries in database")
         try:
-            export_file = open("Chaja's work.xlsx", "wb")
+            export_file = open(f"{username}-work.xlsx", "wb")
         except FileNotFoundError:
             print("File not found")
-        for r in report:
-            r.to_excel(export_file)
+        report.to_excel(export_file)
+        print(f"Successfully exported to {username}-work.xlsx")
 
     if args.repeat:
         for r in all_data:
